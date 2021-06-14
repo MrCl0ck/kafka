422cc2d816bece49238478a464c7f1a32dec1f17
MrCl0ck <levipedreira27@gmail.com>
2021-06-14 10:08:05 -0300

    [+] Adição de classe para consumir todos os tópicos, classe de LOG [+] Adição da classe consumidora Email

--- src/main/java/br/com/ecommerce/maven/NewOrderMain.java
+++ src/main/java/br/com/ecommerce/maven/NewOrderMain.java
@@ -5,6 +5,7 @@ import java.util.Properties;
 import java.util.UUID;
 import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.Callback;
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
@@ -19,13 +20,17 @@ public class NewOrderMain {
 		
 		
 		
-		producer.send(record, (data,ex) -> {
+		Callback callback = (data,ex) -> {
 			if(ex != null) {
 				ex.printStackTrace();
 				return;
 			}
 			System.out.println("Sucesso enviando... "  + data.topic() + ":::partition " + data.partition() + " /offset " + data.offset() + " /timestamp " + data.timestamp());
-		}).get();
+		};
+		var email = "Thank you for your order! We are processing your order"; 
+		var emailRecord = new ProducerRecord<>("ECOMMERCE_SEND_EMAIL", key, email);
+		producer.send(record, callback).get();		
+		producer.send(emailRecord, callback).get();
 		
 	}
 
