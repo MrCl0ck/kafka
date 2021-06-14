d7975f02c6931beffd7a93b9ba74561816310ef0
MrCl0ck <levipedreira27@gmail.com>
2021-06-14 09:18:06 -0300

    [+] Adição de classe para simular o envio de uma mensagem. [+] Adição de uma classe consumidora que irá ouvir a cada vez que uma nova mensagem for produzida.

--- src/main/java/br/com/ecommerce/maven/NewOrderMain.java
+++ src/main/java/br/com/ecommerce/maven/NewOrderMain.java
@@ -1,5 +1,43 @@
 package br.com.ecommerce.maven;
 
-public class NewOrderMain {
+import java.util.Map;
+import java.util.Properties;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class NewOrderMain {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		var producer = new KafkaProducer<String, String>(properties());
+		var value = UUID.randomUUID().toString();
+		var key = "558749-6327";
+		var record = new ProducerRecord<String, String>("ECOMMERCE_NEW_ORDER", value, key);
+		
+		
+		
+		producer.send(record, (data,ex) -> {
+			if(ex != null) {
+				ex.printStackTrace();
+				return;
+			}
+			System.out.println("Sucesso enviando... "  + data.topic() + ":::partition " + data.partition() + " /offset " + data.offset() + " /timestamp " + data.timestamp());
+		}).get();
+		
+	}
+
+	private static Properties properties() {
+		var properties = new Properties();
+		
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		
+		
+		return properties;		
+	}
 }
+
